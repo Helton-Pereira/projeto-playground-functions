25f5cbc8f99f48f7d6bb0df3104d073e2bc1d0c2
Helton Pereira <helton.bfsm@gmail.com>
2022-05-18 15:41:35 -0300

    Desafio 3 resolovido

--- src/challenges.js
+++ src/challenges.js
@@ -11,8 +11,10 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+  let array = [];
+  array = string.split(" ");
+  return array;
 }
 
 // Desafio 4
